eb7b75536ec1c48c671a0dad5f3acc116bc8faf2
Valery <gitval10@mail.ru>
2025-08-24 15:49:12 +0300

    HW-THEME-react-6.18

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
-import './style/main.css';
+//import './style/main.css';
+import './style/main.scss'
 import ErrorBoundary from './components/ErrorBoundary.jsx';
 //import { BrowserRouter } from 'react-router-dom';
 import store from './store'
 import { Provider } from 'react-redux'
 import Sidebar from './components/Sidebar.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
 // Render your React component instead
 import { initReactI18next } from 'react-i18next';
 import i18next from 'i18next';
